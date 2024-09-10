96422b7b609f6e9f857e8c3b89f0f4dde1d21086
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2024-09-10 12:09:47 +0500

    car

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 const app = require('express')();
 const axios = require('axios');
+var bodyParser = require('body-parser')
 
-const host = '127.0.0.1';
+const host = '172.17.10.4';
 const port = 8000;
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//app.use(bodyParser.json())
+
 async function login(token) {
-    try {        
-        const response = await axios.get(`https://hst-api.org.sputnik.vision/wialon/ajax.html?svc=token/login&params={"token":"${token}"}`);
-        //console.log(response.data.eid);
-        return {sid: response.data.eid};
-      } catch (error) {
-        return error.response.body;
-      }  
+  try {
+    const response = await axios.get(`https://hst-api.org.sputnik.vision/wialon/ajax.html?svc=token/login&params={"token":"${token}"}`);
+    //console.log(response.data.eid);
+    return { sid: response.data.eid };
+  } catch (error) {
+    return error.response.body;
+  }
+};
+
+async function car(bodyFormData) {
+  try {
+    const response = await axios({
+      method: "post",
+      url: "https://hst-api.org.sputnik.vision/wialon/ajax.html",
+      data: bodyFormData,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+    // .post(`https://hst-api.org.sputnik.vision/wialon/ajax.html`);
+    //console.log(response.data.items);
+    return response.data;
+  } catch (error) {
+    return error.response.body;
+  }
 };
 
 app.get('/login/:token', async (req, res) => {
-    console.log(req.params.token);
-    const token = req.params.token;
-    const sid = await login(token);
-    //console.log(data);
-    res.json(sid);    
+  console.log(req.params.token);
+  const token = req.params.token;
+  const sid = await login(token);
+  //console.log(data);
+  res.json(sid);
 });
 
+app.post('/car', async function (req, res) {
+
+  console.log("get");
+  const items = await car(req.body);
+  // console.log(items[0]);
+  console.log("items");
+
+  res.send(items);
+});
+
+
+
 app.listen(port, host, function () {
-    console.log(`Server listens http://${host}:${port}`);
+  console.log(`Server listens http://${host}:${port}`);
 });
 
 
