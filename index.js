5add95bdc7b7b917706a84c1ba1a961fb2c755f0
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2024-09-10 14:41:58 +0500

    report

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ async function login(token) {
   try {
     const response = await axios.get(`https://hst-api.org.sputnik.vision/wialon/ajax.html?svc=token/login&params={"token":"${token}"}`);
     //console.log(response.data.eid);
-    return { sid: response.data.eid };
+    return response.data;
   } catch (error) {
     return error.response.body;
   }
 };
 
-async function car(bodyFormData) {
+async function sendPost(bodyFormData) {
   try {
     const response = await axios({
       method: "post",
@@ -40,25 +40,17 @@ app.get('/login/:token', async (req, res) => {
   const token = req.params.token;
   const sid = await login(token);
   //console.log(data);
-  res.json(sid);
+  res.send(sid);
 });
 
-app.post('/car', async function (req, res) {
-
-  console.log("get");
-  const items = await car(req.body);
+app.post('/report', async function (req, res) {
+  // console.log("get");
+  const items = await sendPost(req.body);
   // console.log(items[0]);
-  console.log("items");
-
+  // console.log("items");
   res.send(items);
 });
-
-
 
 app.listen(port, host, function () {
   console.log(`Server listens http://${host}:${port}`);
 });
-
-
-
-
